513db162f4540ab60e3c90009c6fed568a5a25d1
rabi1924 <rabi2024@gmail.com>
2022-03-22 15:07:19 +0900

    Rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}\z/i }
   validates :nickname, presence: true
   A = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,14 @@ FactoryBot.define do
       person { Gimei.name }
     end
 
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
-    password_confirmation {password}
-    last_name             {person.last.kanji}
-    first_name            {person.first.kanji}
-    last_name_kana        {person.last.katakana}
-    first_name_kana       {person.first.katakana}
-    birth_day             {Faker::Date.birthday}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birth_day             { Faker::Date.birthday }
   end
 end
